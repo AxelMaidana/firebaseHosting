f80df09246a47556e344d0aff44cd24d7764a2a6
Axel Maidana <maidana.axel2003@gmail.com>
2024-09-16 09:06:01 -0300

    firebaseHosting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   server: {
     host: 'localhost',
     port: 5173,
@@ -10,12 +12,16 @@ export default defineConfig({
   },
   
   plugins: [react()],
+  
   resolve: {
     alias: {
       'css': path.resolve(__dirname, './src/css'),
       'icons': path.resolve(__dirname, './src/assets/icons'),
       'fonts': path.resolve(__dirname, './src/assets/fonts'),
-
     },
   },
+
+  build: {
+    outDir: 'build', // Cambia la carpeta de salida a 'build'
+  }
 })
